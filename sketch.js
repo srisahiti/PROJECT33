6b62ef9471c8986b6f6e11b07462cf091bc30f53
srisahiti <71134221+srisahiti@users.noreply.github.com>
2020-11-25 16:20:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var particle;
+
+var score = 0;
+var count = 0;
+
+var gameState = "start";
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+  text("Score : "+score,20,30);
+  text("500",25,550);
+  text("500",110,550);
+  text("500",185,550);
+  text("500",265,550);
+  text("100",345,550);
+  text("100",420,550);
+  text("100",510,550);
+  text("200",590,550);
+  text("200",670,550);
+  text("200",750,550);
+  Engine.update(engine);
+
+ 
+
+  if(gameState === "end") {
+    text("GAME OVER",150,200);
+  }
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
+
+function mousePressed(){
+  if(gameState !== "end"){
+    count++;
+    particle = new Particle(mouseX ,10,10,10);
+
+  }
+}
